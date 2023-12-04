d87ade7633dc6630480d0753bece6371b6f1b847
Hilko Wiards <17408906+hwiards@users.noreply.github.com>
2023-12-04 21:17:37 +0100

    Day 4 small improvements

--- solutions/day4.py
+++ solutions/day4.py
@@ -12,60 +12,58 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
-def calc_part1(input):
 
+def calc_part1(input):
     points = 0
     for line in input.splitlines():
         winning_numbers = list(map(int, line.split("|")[0].split(":")[1].strip().split()))
         my_numbers = list(map(int, line.split("|")[1].split()))
         num_winners = len(set(winning_numbers).intersection(set(my_numbers)))
-        points += 2**(num_winners-1) if num_winners > 0 else 0
+        points += 2 ** (num_winners - 1) if num_winners > 0 else 0
 
     return points
+
 
 def part1():
     erg = calc_part1(example.strip())
     print(f"Example Part 1: {erg}")
-    #assert erg == 4361
     print(f"Result Part 1: {calc_part1(input)}")
-
 
 
 def calc_part2(input):
     number_of_processed_cards = 0
 
     lines = input.splitlines()
-    new_backlog_for_card = {}
+    card_to_winnings_map = {}
 
-    card_counter = { i:1 for i in range(1, len(lines)+1)}
+    card_counter = {i: 1 for i in range(1, len(lines) + 1)}
     card_number = 1
+
+    # Loop while the card_counter is not empty
     while sum(card_counter.values()):
-        if card_number not in new_backlog_for_card:
-            line = lines[card_number-1]
+        # Build Map of card to winning cards if not exist
+        if card_number not in card_to_winnings_map:
+            line = lines[card_number - 1]
             winning_numbers = list(map(int, line.split("|")[0].split(":")[1].strip().split()))
             my_numbers = list(map(int, line.split("|")[1].split()))
             num_winners = len(set(winning_numbers).intersection(set(my_numbers)))
-            new_backlog = [card_number + i for i in range(1, num_winners+1)]
-            new_backlog_for_card[card_number] = new_backlog
-
+            card_backlog = [card_number + i for i in range(1, num_winners + 1)]
+            card_to_winnings_map[card_number] = card_backlog
 
         card_count = card_counter[card_number]
+        won_cards = card_to_winnings_map[card_number]
+        for won_card in won_cards:
+            card_counter[won_card] += card_count
+
         card_counter[card_number] = 0
-        backlog = new_backlog_for_card[card_number]
-        for backlog_card in backlog:
-            card_counter[backlog_card] = card_counter[backlog_card] + card_count
-
-
         number_of_processed_cards += card_count
-        if card_number == len(lines):
-            card_number = 1
-        else:
-            card_number += 1
+        # Looping card number
+        card_number = card_number + 1 if card_number != len(lines) else 1
 
     return number_of_processed_cards
+
 
 def part2():
     erg = calc_part2(example)
     print(f"Example Part 2: {erg}")
-    #assert erg == 467835
     print(f"Result Part 2: {calc_part2(input)}")
