1f57c6f785edaa223ce4868486b14159a97e9f16
Hilko Wiards <17408906+hwiards@users.noreply.github.com>
2023-12-09 16:28:00 +0100

    Day 9, small improvements

--- solutions/day9.py
+++ solutions/day9.py
@@ -1,54 +1,46 @@
 import os
-from input_loader import *
-from helpers import *
+from input_loader import load_input_str
 from itertools import pairwise
-input = load_input_str(os.path.basename(__file__)[:-3])
+
+input_str = load_input_str(os.path.basename(__file__)[:-3])
 
 example = """0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45"""
 
+
 def calc_next_value(sequence):
-    diffs = [b-a for a, b in pairwise(sequence)]
-    if all(i == 0 for i in diffs):
+    if all(i == 0 for i in sequence):
         # end condition reached
-        sequence.append(sequence[-1])
-        return sequence
-    else:
-        longer_diff = calc_next_value(diffs)
-        sequence.append(sequence[-1] + longer_diff[-1])
         return sequence
 
+    diffs = [b - a for a, b in pairwise(sequence)]
+    extended_diff = calc_next_value(diffs)
+    sequence.append(sequence[-1] + extended_diff[-1])
+    return sequence
 
 
-def calc_part1(input):
+def calc(input_str, part2=False):
     next_values = []
-    for line in input.splitlines():
-        line = list(map(int, line.split()))
+    for line_str in input_str.splitlines():
+        line = [int(i) for i in line_str.split()]
+        if part2:
+            line.reverse()
         longer_sequence = calc_next_value(line)
         next_values.append(longer_sequence[-1])
 
     return sum(next_values)
+
 
 def part1():
-    erg = calc_part1(example.strip())
+    erg = calc(example.strip())
     print(f"Example Part 1: {erg}")
-    #assert erg == 4361
-    print(f"Result Part 1: {calc_part1(input)}")
+    assert erg == 114
+    print(f"Result Part 1: {calc(input_str)}")
 
-
-def calc_part2(input):
-    next_values = []
-    for line in input.splitlines():
-        line = list(map(int, line.split()))
-        line.reverse()
-        longer_sequence = calc_next_value(line)
-        next_values.append(longer_sequence[-1])
-
-    return sum(next_values)
 
 def part2():
-    erg = calc_part2(example)
+    erg = calc(example, part2=True)
     print(f"Example Part 2: {erg}")
-    #assert erg == 467835
-    print(f"Result Part 2: {calc_part2(input)}")
+    assert erg == 2
+    print(f"Result Part 2: {calc(input_str, part2=True)}")
