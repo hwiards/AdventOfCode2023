0a0b50dc7006770b83d8a245e7de5e4e04c05221
Hilko Wiards <17408906+hwiards@users.noreply.github.com>
2023-12-10 16:40:14 +0100

    Day 10, little cleanup

--- solutions/day0.py
+++ solutions/day0.py
@@ -1,27 +1,27 @@
 import os
-from input_loader import *
+from input_loader import load_input_str
 from helpers import *
 
-input = load_input_str(os.path.basename(__file__)[:-3])
+input_str = load_input_str(os.path.basename(__file__)[:-3])
 
 example = """
 """
 
-def calc_part1(input):
+def calc_part1(input_str):
     pass
 
 def part1():
     erg = calc_part1(example.strip())
     print(f"Example Part 1: {erg}")
     #assert erg == 4361
-    print(f"Result Part 1: {calc_part1(input)}")
+    print(f"Result Part 1: {calc_part1(input_str)}")
 
 
-def calc_part2(input):
+def calc_part2(input_str):
     pass
 
 def part2():
     erg = calc_part2(example)
     print(f"Example Part 2: {erg}")
     #assert erg == 467835
-    print(f"Result Part 2: {calc_part2(input)}")
+    print(f"Result Part 2: {calc_part2(input_str)}")
--- solutions/day10.py
+++ solutions/day10.py
@@ -1,8 +1,8 @@
 import os
-from input_loader import *
-from helpers import *
+from input_loader import load_input_str
+from helpers import tadd, pad
 
-input = load_input_str(os.path.basename(__file__)[:-3])
+input_str = load_input_str(os.path.basename(__file__)[:-3])
 
 example = """.....
 .S-7.
@@ -10,13 +10,13 @@ example = """.....
 .L-J.
 ....."""
 
-EXAMPLE2="""..F7.
+EXAMPLE2 = """..F7.
 .FJ|.
 SJ.L7
 |F--J
 LJ..."""
 
-EXAMPLE3="""...........
+EXAMPLE3 = """...........
 .S-------7.
 .|F-----7|.
 .||.....||.
@@ -37,7 +37,6 @@ L---JF-JLJ.||-FJLJJ7
 L.L7LFJ|||||FJL7||LJ
 L7JLJL-JLJLJL--JLJ.L"""
 
-
 EXAMPLE5 = """.F----7F7F7F7F-7....
 .|F--7||||||||FJ....
 .||.FJ||||||||L7....
@@ -49,21 +48,20 @@ L--J.L7...LJS7F-7L7.
 ....FJL-7.||.||||...
 ....L---J.LJ.LJLJ..."""
 
-DIRs = [(1,0), (0,1), (-1,0), (0, -1)]
+DIRs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 pipes = {
     "|": ((-1, 0), (1, 0)),
     "-": ((0, -1), (0, 1)),
     "L": ((-1, 0), (0, 1)),
     "J": ((-1, 0), (0, -1)),
     "7": ((1, 0), (0, -1)),
-    "F": ((1, 0), (0, 1))
+    "F": ((1, 0), (0, 1)),
 }
 
-def calc_part1(input):
-    padded = pad(input, ".")
+
+def calc(input_str, part2=False):
+    padded = pad(input_str, ".")
     lines = padded.splitlines()
-
-
 
     S = None
     for i, line in enumerate(lines):
@@ -75,16 +73,17 @@ def calc_part1(input):
             continue
         break
 
-    for dir in DIRs:
-        y_shift, x_shift = dir
-        pipe_symbol = lines[S[0]+y_shift][S[1]+x_shift]
+    for direction in DIRs:
+        y_shift, x_shift = direction
+        pipe_symbol = lines[S[0] + y_shift][S[1] + x_shift]
         if pipe_symbol in pipes:
             connect1, connect2 = pipes[pipe_symbol]
-            if all(i == 0 for i in tadd(dir, connect1)) or all(i == 0 for i in tadd(dir, connect2)):
-                act_dir = dir
+            if all(i == 0 for i in tadd(direction, connect1)) or all(
+                i == 0 for i in tadd(direction, connect2)
+            ):
+                act_dir = direction
                 break
 
-    steps = 0
     memory = []
     act_pos = S
     while True:
@@ -93,160 +92,66 @@ def calc_part1(input):
         next_pipe = lines[next_y][next_x]
         if next_pipe == "S":
             break
-        next_dir = pipes[next_pipe][0] if not all(i == 0 for i in tadd(act_dir, pipes[next_pipe][0])) else pipes[next_pipe][1]
+        next_dir = (
+            pipes[next_pipe][0]
+            if not all(i == 0 for i in tadd(act_dir, pipes[next_pipe][0]))
+            else pipes[next_pipe][1]
+        )
         act_pos = (next_y, next_x)
         act_dir = next_dir
 
-    return len(memory)//2
+    if not part2:
+        return len(memory) // 2
 
+    lines = replace_s(lines, memory)
+    return check_inside_loop(lines, memory)
 
-def part1():
-    erg = calc_part1(example.strip())
-    print(f"Example Part 1: {erg}")
-    erg = calc_part1(EXAMPLE2.strip())
-    print(f"Example Part 1_2: {erg}")
-    #assert erg == 4361
-    print(f"Result Part 1: {calc_part1(input)}")
-
-
-def get_elems_around(i,j):
-    elems = []
-    for dir in DIRs:
-        y,x = dir
-        elems.append((i+y, j+x))
-    return elems
-
-# def region_check(lines, memory):
-#     not_enclosed = set()
-#     enclosed = set()
-#     memory = set(memory)
-#     to_check = set()
-#     checked = set()
-#     for i, line in enumerate(lines):
-#         for j, symbol in enumerate(line):
-#             checked.add((i, j))
-#             if symbol == "#":
-#                 not_enclosed.add((i,j))
-#                 continue
-#             if (i, j) in memory:
-#                 continue
-#
-#             if any(elem_around in not_enclosed for elem_around in get_elems_around(i, j)):
-#                 not_enclosed.add((i, j))
-#                 continue
-#
-#             to_check.add((i,j))
-#
-#     while True:
-#         changed = False
-#         for y, x in to_check:
-#             if any(elem_around in not_enclosed for elem_around in get_elems_around(y, x)):
-#                 not_enclosed.add((y, x))
-#                 to_check.remove((y,x))
-#                 changed = True
-#                 break
-#         if not changed:
-#             break
-#
-#
-#     enclosed = checked.difference(memory).difference(not_enclosed)
-#     return len(enclosed)
 
 def check_inside_loop(lines, loop):
     loop = set(loop)
-
     enclosed = set()
 
     for i, line in enumerate(lines):
         for j, symbol in enumerate(line):
             if (i, j) in loop or symbol == "#":
                 continue
-            inter_y_1 = [(y, j) for y in range(i) if (y, j) in loop and lines[y][j] == "-"]
-            inter_y_2 = [(y, j) for y in range(i, len(lines)) if (y, j) in loop and lines[y][j] == "-"]
-            inter_x_1 = [(i, x) for x in range(j) if (i, x) in loop and lines[i][x] in ["|", "L", "J", "S"]]
-            inter_x_2 = [(i, x) for x in range(j, len(line)) if (i, x) in loop and lines[i][x] in ["|", "L", "J", "S"]]
+            # Count the intersections toward the left side.
+            # If it is odd, then it is within the loop
+            inter_x = [
+                (i, x)
+                for x in range(j)
+                if (i, x) in loop and lines[i][x] in ["|", "L", "J"]
+            ]
 
-            #if any(len(inter) % 2 == 1 for inter in [inter_x_1]):
-            #    enclosed.add((i, j))
-
-            if len(inter_x_1) % 2 == 1:
-                enclosed.add((i,j))
+            if len(inter_x) % 2 == 1:
+                enclosed.add((i, j))
 
     return len(enclosed)
 
-def is_valid(lines, x, y, visited, loop):
-    return lines[y][x] != "#" and (y,x) not in loop and visited[y][x] == 0
 
-#def flood_fill(lines, x, y, visited, loop):
-#    visited[y][x] =True
-#    for dy, dx in DIRs:
-#        next_y, next_x = y+dy, x+dx
-#        if is_valid(lines, next_x, next_y, visited):
-
-
-#def get_enclosed_points(lines, loop):
-#    enclosed = set()
-#    not_enclosed = set()
-#    visited = [[ for _ in range(len(lines[0]))] for _ in range(len(lines))]
-
-
-
-def replace_L(lines, loop):
+def replace_s(lines, loop):
     starty, startx = loop[0]
     for pipe, (dir1, dir2) in pipes.items():
-        if (starty + dir1[0], startx+dir1[1]) in loop and (starty + dir2[0], startx+dir2[1]) in loop:
-            newline = lines[starty][:startx] + pipe + lines[starty][startx+1:]
+        dir_in_loop1 = (starty + dir1[0], startx + dir1[1]) in loop
+        dir_in_loop2 = (starty + dir2[0], startx + dir2[1]) in loop
+        if dir_in_loop1 and dir_in_loop2:
+            newline = lines[starty][:startx] + pipe + lines[starty][startx + 1 :]
             lines[starty] = newline
 
     return lines
 
-def calc_part2(input):
-    padded = pad(input, "#")
-    lines = padded.splitlines()
 
-    S = None
-    for i, line in enumerate(lines):
-        for j, symbol in enumerate(line):
-            if symbol == "S":
-                S = (i, j)
-                break
-        else:
-            continue
-        break
-
-
-    for dir in DIRs:
-        y_shift, x_shift = dir
-        pipe_symbol = lines[S[0]+y_shift][S[1]+x_shift]
-        if pipe_symbol in pipes:
-            connect1, connect2 = pipes[pipe_symbol]
-            if all(i == 0 for i in tadd(dir, connect1)) or all(i == 0 for i in tadd(dir, connect2)):
-                act_dir = dir
-                break
-
-    steps = 0
-    memory = []
-    memory_lines= []
-    act_pos = S
-    while True:
-        memory.append(act_pos)
-        act_pipe = lines[act_pos[0]][act_pos[1]]
-        if act_pipe not in ["L", "J","7","F"]:
-            memory_lines.append(act_pos)
-        next_y, next_x = tadd(act_pos, act_dir)
-        next_pipe = lines[next_y][next_x]
-        if next_pipe == "S":
-            break
-        next_dir = pipes[next_pipe][0] if not all(i == 0 for i in tadd(act_dir, pipes[next_pipe][0])) else pipes[next_pipe][1]
-        act_pos = (next_y, next_x)
-        act_dir = next_dir
-
-    lines = replace_L(lines, memory)
-    return check_inside_loop(lines, memory)
+def part1():
+    erg = calc(example.strip())
+    print(f"Example Part 1: {erg}")
+    erg = calc(EXAMPLE2.strip())
+    print(f"Example Part 1_2: {erg}")
+    assert erg == 8
+    print(f"Result Part 1: {calc(input_str)}")
 
 
 def part2():
-    erg = calc_part2(EXAMPLE5)
+    erg = calc(EXAMPLE4, part2=True)
     print(f"Example Part 2: {erg}")
-    #assert erg == 467835
-    print(f"Result Part 2: {calc_part2(input)}")
+    assert erg == 10
+    print(f"Result Part 2: {calc(input_str, part2=True)}")
