5628dd6d6c8cf0cfe8c2f30ce2d90edb4e4abbbb
Hilko Wiards <17408906+hwiards@users.noreply.github.com>
2023-12-13 16:31:51 +0100

    Day 13, initial solution

--- input_loader.py
+++ input_loader.py
@@ -1,10 +1,14 @@
 
 
 def load_input_str(filename : str):
-    with open(f"inputs/{filename}.txt", 'r') as file:
-        # Read the entire content of the file into a string
-        file_contents = file.read()
-        return file_contents.strip()
+    try:
+        with open(f"inputs/{filename}.txt", 'r') as file:
+            # Read the entire content of the file into a string
+            file_contents = file.read()
+            return file_contents.strip()
+    except FileNotFoundError:
+        return None
+
 
 def load_input_lines(filename: str):
     input = load_input_str(filename)
---
+++ solutions/day13.py
@@ -0,0 +1,114 @@
+import os
+from input_loader import load_input_str
+from helpers import *
+
+input_str = load_input_str(os.path.basename(__file__)[:-3])
+
+example = """#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#"""
+
+
+def cal_diff(line1, line2):
+    num_diff = 0
+    for a, b in zip(list(line1), list(line2)):
+        if a != b:
+            num_diff += 1
+
+    return num_diff
+
+def calc_notes2(pattern):
+    pattern = pattern.splitlines()
+
+    #horizontal
+    for split_idx in range(1, len(pattern)):
+        diffs = 0
+        for i in range(split_idx):
+            idx_lower = split_idx - i -1
+            idx_upper = split_idx + i
+            if idx_lower >= 0 and idx_upper < len(pattern):
+                diffs += cal_diff(pattern[idx_lower], pattern[idx_upper])
+
+        if diffs == 1:
+            return split_idx * 100
+
+    for split_idx in range(1, len(pattern[0])):
+        diffs = 0
+        for i in range(split_idx):
+
+            idx_lower = split_idx - i - 1
+            idx_upper = split_idx + i
+            if idx_lower < 0 or idx_upper >= len(pattern[0]):
+                continue
+            col_lower = "".join(a[idx_lower] for a in pattern)
+            col_upper = "".join(a[idx_upper] for a in pattern)
+            diffs += cal_diff(col_upper, col_lower)
+
+        if diffs == 1:
+            return split_idx
+
+    return 0
+
+
+def calc_notes(pattern):
+    pattern = pattern.splitlines()
+
+    #horizontal
+    for split_idx in range(1, len(pattern)):
+        for i in range(split_idx):
+            idx_lower = split_idx - i -1
+            idx_upper = split_idx + i
+            if idx_lower >= 0 and idx_upper < len(pattern) and pattern[idx_lower] != pattern[idx_upper]:
+                break
+        else:
+            return split_idx * 100
+
+    for split_idx in range(1, len(pattern[0])):
+        for i in range(split_idx):
+
+            idx_lower = split_idx - i - 1
+            idx_upper = split_idx + i
+            if idx_lower < 0 or idx_upper >= len(pattern[0]):
+                continue
+            col_lower = "".join(a[idx_lower] for a in pattern)
+            col_upper = "".join(a[idx_upper] for a in pattern)
+            if  col_upper != col_lower:
+                break
+        else:
+            return split_idx
+
+    return 0
+
+
+def calc_part1(input_str):
+    patterns = input_str.split("\n\n")
+    return sum(calc_notes(pattern) for pattern in patterns)
+
+def part1():
+    erg = calc_part1(example.strip())
+    print(f"Example Part 1: {erg}")
+    assert erg == 405
+    print(f"Result Part 1: {calc_part1(input_str)}")
+
+
+def calc_part2(input_str):
+    patterns = input_str.split("\n\n")
+    return sum(calc_notes2(pattern) for pattern in patterns)
+
+def part2():
+    erg = calc_part2(example)
+    print(f"Example Part 2: {erg}")
+    assert erg == 400
+    print(f"Result Part 2: {calc_part2(input_str)}")
