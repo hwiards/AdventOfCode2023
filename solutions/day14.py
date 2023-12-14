bb98b3af054ce53f028b21a9fe3f788c1a084ed4
Hilko Wiards <17408906+hwiards@users.noreply.github.com>
2023-12-14 19:33:26 +0100

    Day14 cleanup

--- solutions/day14.py
+++ solutions/day14.py
@@ -1,6 +1,5 @@
 import os
 from input_loader import load_input_str
-from helpers import *
 
 input_str = load_input_str(os.path.basename(__file__)[:-3])
 
@@ -15,42 +14,20 @@ O.#..O.#.#
 #....###..
 #OO..#...."""
 
-def calc_part1(input_str):
-    lines = input_str.splitlines()
 
-    for i, line in enumerate(lines):
-        if i == 0:
-            continue
-
-        for j, c in enumerate(line):
-            if c == "O":
-                last_possible_row = i
-                for x in range(1, i+1):
-                    if lines[i-x][j] == ".":
-                        last_possible_row = i-x
-                    else:
-                        break
-                if last_possible_row != i:
-                    lines[last_possible_row] = lines[last_possible_row][:j] + "O" + lines[last_possible_row][j+1:]
-                    lines[i] = lines[i][:j] + "." + lines[i][j+1:]
-
+def calc_load(state):
     load = 0
-    for i, line in enumerate(lines):
-        load += (len(lines)-i) * list(line).count("O")
-
+    for i, line in enumerate(state):
+        load += (len(state) - i) * list(line).count("O")
 
     return load
 
 
-
-def part1():
-    erg = calc_part1(example.strip())
-    print(f"Example Part 1: {erg}")
-    assert erg == 136
-    print(f"Result Part 1: {calc_part1(input_str)}")
+def rotate(state):
+    return tuple(row for row in zip(*reversed(state)))
 
 
-def rotate_and_shift(state):
+def tilt_north(state):
     state = [list(row) for row in state]
 
     for i, line in enumerate(state):
@@ -69,47 +46,40 @@ def rotate_and_shift(state):
                     state[last_possible_row][j] = "O"
                     state[i][j] = "."
 
-
-
-    state = tuple(row for row in zip(*reversed(state)))
     return state
 
 
+def calc_part1(input_str: str):
+    state = input_str.splitlines()
+    state = tilt_north(state)
+    return calc_load(state)
+
+
+def part1():
+    erg = calc_part1(example.strip())
+    print(f"Example Part 1: {erg}")
+    assert erg == 136
+    print(f"Result Part 1: {calc_part1(input_str)}")
+
 
 def calc_part2(input_str):
-
     lines = input_str.splitlines()
     state = [list(line) for line in lines]
 
     previous_states = {}
-    for i in range (4_000_000_000):
-        state = rotate_and_shift(state)
+    for i in range(4 * 1_000_000_000):
+        state = rotate(tilt_north(state))
         if state in previous_states:
             break
         previous_states[state] = i
-        if i in [3, 7, 11]:
-            for line in state:
-                for c in line:
-                    print(c, end="")
-                print()
-            print()
-
-
 
     shift = i - previous_states[state]
     num_cycles_left = (4_000_000_000 - i) % shift
 
-    for _ in range(num_cycles_left-1):
-        state = rotate_and_shift(state)
+    for _ in range(num_cycles_left - 1):
+        state = rotate(tilt_north(state))
 
-
-
-    load = 0
-    for i, line in enumerate(state):
-        load += (len(lines)-i) * line.count("O")
-
-
-    return load
+    return calc_load(state)
 
 
 def part2():
