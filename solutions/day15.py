58049ae33cb0c5212c71a9f53e7fc784c6bb35fb
Hilko Wiards <17408906+hwiards@users.noreply.github.com>
2023-12-15 17:59:26 +0100

    Day 15, changed list to map

--- solutions/day15.py
+++ solutions/day15.py
@@ -1,18 +1,16 @@
 import os
 from input_loader import load_input_str
-from helpers import *
 from collections import defaultdict
 
 input_str = load_input_str(os.path.basename(__file__)[:-3])
 
-
 example = """rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"""
+
 
 def calc_hash(seq: str):
     cur = 0
     for c in seq:
-        o = ord(c)
-        cur += o
+        cur += ord(c)
         cur *= 17
         cur %= 256
 
@@ -20,42 +18,39 @@ def calc_hash(seq: str):
 
 
 def calc_part1(input_str):
-    return sum (calc_hash(x) for x in input_str.split(","))
+    return sum(calc_hash(x) for x in input_str.split(","))
+
 
 def part1():
     erg = calc_part1(example.strip())
     print(f"Example Part 1: {erg}")
-    #assert erg == 4361
+    assert erg == 1320
     print(f"Result Part 1: {calc_part1(input_str)}")
 
 
 def calc_part2(input_str):
     seqs = input_str.split(",")
 
-    boxes = defaultdict(list)
+    boxes = defaultdict(dict)
     for seq in seqs:
         if "-" in seq:
             label = seq[:-1]
             box = calc_hash(label)
-            boxes[box] = [(labelo, lenso) for (labelo, lenso) in boxes[box] if labelo != label]
+            boxes[box].pop(label, None)
 
         elif "=" in seq:
             label, lens = seq.split("=")
             lens = int(lens)
             box = calc_hash(label)
-            if label in [labelo for (labelo, _) in boxes[box]]:
-                boxes[box] = [(label, lens) if labelo == label else (labelo, lenso) for (labelo, lenso) in boxes[box]]
-            else:
-                boxes[box].append((label, lens))
+            boxes[box][label] = lens
 
     power = 0
     for box, box_elems in boxes.items():
-        for slot, (label, lens) in enumerate(box_elems,1):
-            focal_power = (box+1)* slot * lens
-            power += focal_power
-
+        for slot, lens in enumerate(box_elems.values(), 1):
+            power += (box + 1) * slot * lens
 
     return power
+
 
 def part2():
     erg = calc_part2(example)
