1c7066a15da5333a1a04a90d3ba662e9823abac3
Hilko Wiards <17408906+hwiards@users.noreply.github.com>
2023-12-20 22:14:35 +0100

    Day 20 cleanup

--- solutions/day20.py
+++ solutions/day20.py
@@ -1,8 +1,8 @@
 import math
 import os
 from input_loader import load_input_str
-from helpers import *
 from collections import defaultdict
+
 input_str = load_input_str(os.path.basename(__file__)[:-3])
 
 example = """broadcaster -> a, b, c
@@ -11,72 +11,24 @@ example = """broadcaster -> a, b, c
 %c -> inv
 &inv -> a"""
 
-
-example2="""broadcaster -> a
+example2 = """broadcaster -> a
 %a -> inv, con
 &inv -> b
 %b -> con
 &con -> output"""
 
-def press_button(state, state_conjunct, instructions:dict[str, tuple], start="broadcaster", times=1000):
-    highs, lows = 0, 0
 
-    for _ in range(times):
-        stack = [("broadcaster", 0, None)]
-        lows += 1
-        while stack:
-            seq_elem, signal, sender = stack.pop(0)
-
-            if seq_elem not in instructions:
-                continue
-            destinations, typ = instructions[seq_elem]
-
-            if typ is None:
-                lows += len(destinations)
-                for dest in destinations:
-                    #state[dest] = 0
-                    stack.append((dest, 0, seq_elem))
-
-
-            if typ == "%":
-                if signal == 1:
-                    continue
-                new_state = 0 if state[seq_elem] else 1
-                if new_state:
-                    highs += len(destinations)
-                else:
-                    lows += len(destinations)
-                for dest in destinations:
-                    #state[dest] = new_state
-                    stack.append((dest, new_state, seq_elem))
-                state[seq_elem] = new_state
-
-
-            if typ == "&":
-                state_conjunct[seq_elem][sender] = signal
-                new_state = 0 if all(state_conjunct[seq_elem].values()) else 1
-                #new_state = and(1 )
-                #new_state = any(1 if state[modul] == 0 else 0 for modul in state_conjunct[seq_elem])
-                #new_state = 1 if new_state else 0
-                if new_state:
-                    highs += len(destinations)
-                else:
-                    lows += len(destinations)
-                for dest in destinations:
-                    #state[dest] = new_state
-                    stack.append((dest, new_state, seq_elem))
-
-    print(lows, highs)
-    return highs * lows
-
-
-def press_button2 (state, state_conjunct, instructions: dict[str, tuple], start="broadcaster"):
+def press_button(state, state_conjunct, instructions: dict[str, tuple], part2=False):
     highs, lows = 0, 0
 
     vals = {}
     n = 0
     while True:
-        n+=1
+        # Part 1
+        if n == 1000 and not part2:
+            return highs * lows
+
+        n += 1
         stack = [("broadcaster", 0, None)]
         lows += 1
         while stack:
@@ -86,65 +38,42 @@ def press_button2 (state, state_conjunct, instructions: dict[str, tuple], start=
                 continue
             destinations, typ = instructions[seq_elem]
 
-            if seq_elem == "cs" and sender != "broadcaster" and signal == 1:
+            # Part 2
+            if seq_elem == "cs" and signal == 1:
                 if sender not in vals:
                     vals[sender] = n
                 if len(vals) == 4:
                     return math.lcm(*vals.values())
 
-
-
             if typ is None:
-                lows += len(destinations)
-                for dest in destinations:
-                    # state[dest] = 0
-                    stack.append((dest, 0, seq_elem))
+                new_signal = 0
 
             if typ == "%":
                 if signal == 1:
                     continue
-                new_state = 0 if state[seq_elem] else 1
-                if new_state:
-                    highs += len(destinations)
-                else:
-                    lows += len(destinations)
-                for dest in destinations:
-                    # state[dest] = new_state
-                    stack.append((dest, new_state, seq_elem))
-                state[seq_elem] = new_state
+                state[seq_elem] = new_signal = not state[seq_elem]
 
             if typ == "&":
                 state_conjunct[seq_elem][sender] = signal
-                new_state = 0 if all(state_conjunct[seq_elem].values()) else 1
-                # new_state = and(1 )
-                # new_state = any(1 if state[modul] == 0 else 0 for modul in state_conjunct[seq_elem])
-                # new_state = 1 if new_state else 0
-                if new_state:
-                    highs += len(destinations)
-                else:
-                    lows += len(destinations)
-                for dest in destinations:
-                    # state[dest] = new_state
-                    stack.append((dest, new_state, seq_elem))
+                new_signal = not all(state_conjunct[seq_elem].values())
 
-    return 0
+            stack.extend((dest, new_signal, seq_elem) for dest in destinations)
+            highs += len(destinations) if new_signal else 0
+            lows += len(destinations) if not new_signal else 0
 
 
-def calc_part1(input_str):
+def parse(input_str):
     instr = {}
     state = defaultdict(int)
     states_conjunct = {}
 
     for line in input_str.splitlines():
         module, destinations = line.split(" -> ")
-        if module[0] == "%":
+        if module[0] == "%" or module[0] == "&":
             dests = destinations.split(", ")
             instr[module[1:]] = (dests, module[0])
-        elif module[0] == "&":
-            dests = destinations.split(", ")
-            instr[module[1:]] = (dests, module[0])
-            #states_conjunct[module[1:]] = []
-            states_conjunct[module[1:]] = {}
+            if module[0] == "&":
+                states_conjunct[module[1:]] = {}
         else:
             instr[module] = (destinations.split(", "), None)
 
@@ -152,47 +81,26 @@ def calc_part1(input_str):
         for dest in dests:
             if dest in states_conjunct:
                 states_conjunct[dest][modul] = 0
-                #states_conjunct[dest].append(modul)
 
-    return press_button(state, states_conjunct, instr, "broadcaster", 1000)
+    return state, states_conjunct, instr
+
+
+def calc_part1(input_str):
+    state, states_conjunct, instr = parse(input_str)
+    return press_button(state, states_conjunct, instr)
 
 
 def part1():
     erg = calc_part1(example2.strip())
     print(f"Example Part 1: {erg}")
-    #assert erg == 4361
+    assert erg == 11687500
     print(f"Result Part 1: {calc_part1(input_str)}")
 
 
 def calc_part2(input_str):
-    instr = {}
-    state = defaultdict(int)
-    states_conjunct = {}
-
-    for line in input_str.splitlines():
-        module, destinations = line.split(" -> ")
-        if module[0] == "%":
-            dests = destinations.split(", ")
-            instr[module[1:]] = (dests, module[0])
-        elif module[0] == "&":
-            dests = destinations.split(", ")
-            instr[module[1:]] = (dests, module[0])
-            #states_conjunct[module[1:]] = []
-            states_conjunct[module[1:]] = {}
-        else:
-            instr[module] = (destinations.split(", "), None)
-
-    for modul, (dests, typ) in instr.items():
-        for dest in dests:
-            if dest in states_conjunct:
-                states_conjunct[dest][modul] = 0
-                #states_conjunct[dest].append(modul)
-
-    return press_button2(state, states_conjunct, instr, "broadcaster")
+    state, states_conjunct, instr = parse(input_str)
+    return press_button(state, states_conjunct, instr, part2=True)
 
 
 def part2():
-#    erg = calc_part2(example)
-#    print(f"Example Part 2: {erg}")
-    #assert erg == 467835
     print(f"Result Part 2: {calc_part2(input_str)}")
